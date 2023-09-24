c96e6aeaa59c214c6705cbb51774f248eaac5bea
Gabe Tucker <tucker.854@osu.edu>
2023-09-24 18:24:49 -0400

    Final version of feature importance analysis

--- pureFeatureImportance/parameters.py
+++ pureFeatureImportance/parameters.py
@@ -5,7 +5,7 @@ import functions_classBased
 
 # STATIC
 DATASET = datasets.dataset_USNW_NB15
-ANALYSIS_FUNCTION = functions_modelBased.randomForest
+ANALYSIS_FUNCTION = functions_classBased.selectKBest
 OUTPUT_NAME = "importanceGraph"
 
 OUTPUT_FOLDER = "graphs"
