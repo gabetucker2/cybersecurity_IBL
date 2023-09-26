8930054d1bb66f3c3f4456caa48d5ea0257cca6f
Gabe Tucker <tucker.854@osu.edu>
2023-09-26 14:37:28 -0400

    Removed redundant folder

--- combined/functions_helper.py
+++
@@ -1,6 +0,0 @@
-# FUNCTIONS
-def get_percent(probability):
-    return str(round(probability * 100, 2)) + '%'
-
-def array_to_dictionary(arr):
-    return {str(i): value for i, value in enumerate(arr)}
