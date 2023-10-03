3df7837f9e6e7467c29626ec34ef8a3a764d277f
Gabe Tucker <tucker.854@osu.edu>
2023-10-03 18:19:21 -0400

    Made univariate able to handle neg data

--- ACTRFeatureImportance/functions_univariate.py
+++ ACTRFeatureImportance/functions_univariate.py
@@ -1,8 +1,5 @@
-# LIBRARIES
 import pandas as pd
-from sklearn.feature_selection import SelectKBest, chi2
-
-# SCRIPTS
+from sklearn.feature_selection import SelectKBest, chi2, f_classif
 import functions_preprocessing
 
 def univariate_selectKBest():
@@ -10,9 +7,16 @@ def univariate_selectKBest():
     df = functions_preprocessing.numeric_preprocessing(df)
     df = functions_preprocessing.categorical_preprocessing(df)
 
-    best_features = SelectKBest(score_func=chi2, k='all')
     X = df.iloc[:,4:-2]
     y = df.iloc[:,-1]
+    
+    # Check if data has negative values to decide on the scoring function
+    if (X < 0).any().any():
+        score_func = f_classif
+    else:
+        score_func = chi2
+
+    best_features = SelectKBest(score_func=score_func, k='all')
     fit = best_features.fit(X, y)
 
     df_scores = pd.DataFrame(fit.scores_)
